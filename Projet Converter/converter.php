32f29ad2678ce512cf9d5d0289ef03b783e58da9
leondiaye65 <69790367+leondiaye65@users.noreply.github.com>
2020-10-21 08:59:38 +0200

    mise à jour

---
+++ Projet Converter/converter.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <link rel="stylesheet" href="Converter.css" />
+    
+
+
+   
+    
+ <title>Projet Converter</title>
+</head>
+<body>
+<header>
+<div class ="container">
+<h1>Projet Converter</h1>
+</div>
+<br>
+<div class= "row">
+</div>
+<div class="container"
+<h2>Converter Page</h2>
+</div>
+<header>
+<fieldset>
+<label for= "">valeur en 'c'</label>
+<input type="text" name="valeur 'c' " placeholdder= "Entrez la température en 'c'" id="température" value="78.5'c'"></br>
+
+
+</body>
+</html>
---
+++ Projet Converter/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <link rel="stylesheet" href="LOGIN FORM.css" />
+    
+
+
+   
+    
+ <title>LOGIN FORM</title>
+</head>
+<body>
+<header>
+<div class ="container">
+<h1>LOGIN FORM</h1>
+<br>
+<div class= "row">
+</div>
+
+<header>
+<fieldset>
+<label for= "">identifiant</label>
+<input type="text" name="identifiant" placeholdder= "Entrez votre identifiant" id="identifiant" value=""></br>
+<label for="">Mot de passe</label> 
+<input type="text" name="Mot de passe" placeholder= "Entrez votre mot de passe" id="Mot de passe" value=""></br>
+</fieldset>    
+</body>
+</html>
